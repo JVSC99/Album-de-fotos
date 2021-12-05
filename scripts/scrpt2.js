1b11c73fbe271aba9a2c10019db9c5fe348dbfc4
JVSC99 <jvsc76@gmail.com>
2021-12-04 21:24:13 -0300

    Versão final

--- scripts/scrpt2.js
+++ scripts/scrpt2.js
@@ -1,4 +1,8 @@
-for(let i=0;i<=5;i++){
+let num_fotos = 19; // aqui definimos o número de 'espaços' onde as sub imagens vão ficar. 
+//(IMPORTANTE) ao alterar a num_fotos é necessário altera-lá a mesma variavel no script.js linha 15 do código.
+
+// função responsável por criar as tags 'espaços' onde as sub imagens vão ficar.
+for(let i=0;i<=num_fotos;i++){
 
     document.write("<div onclick=colocarNaBig("+i+") style='margin-right: 1%;cursor: pointer;'><img id='img"+i+"' class='sub_img' src='images/leadster_icon.png' alt=''></div>")
 
